2b8e9aff0a3e780ed2ac216cbb8288bb80fd7807
Jakob Fuhrmann <3326760+Jafu@users.noreply.github.com>
2020-09-08 09:21:01 +0200

    Add jest testing

--- src/main.js
+++ src/main.js
@@ -3,12 +3,26 @@ import ReactDOM from "react-dom";
 import { createStore } from 'redux';
 
 
-const store = createStore(reduce);
-store.subscribe(render);
+export const ACTIONS = {
+	INIT: 'INIT',
+};
 
-function reduce(state, { type, data }) {
+
+function createInit() {
+	return { type: ACTIONS.INIT, data: {} };
+}
+
+
+export function init() {
+	const store = createStore(reduce);
+	store.subscribe(render);
+	store.dispatch(createInit);
+}
+
+
+export function reduce(state, { type, data }) {
 	switch(type) {
-		case 'INIT':
+		case ACTIONS.INIT:
 			state = data;
 			break;
 		default:
@@ -16,13 +30,13 @@ function reduce(state, { type, data }) {
 	return state;
 }
 
+
 function render() {
 	var mountNode = document.getElementById("app");
 	ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
 }
 
+
 function HelloMessage({ name }) {
     return <div>Hello {name}</div>;
 }
-
-store.dispatch({ type: 'INIT', data: {} });
---
+++ src/tests/index.test.js
@@ -0,0 +1,5 @@
+import { reduce } from '../main';
+
+test('should assert hello world', () => {
+	expect('hello world');
+});
