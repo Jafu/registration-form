eef258c9c40635be7e77af7a31247e517f102581
Jakob Fuhrmann <3326760+Jafu@users.noreply.github.com>
2020-09-10 22:54:59 +0200

    Render controlled email input with focus

--- src/main.js
+++ src/main.js
@@ -5,18 +5,53 @@ import { createStore } from 'redux';
 
 export const ACTIONS = {
 	INIT: 'INIT',
+	LOGIN_EMAIL_INPUT: 'LOGIN_EMAIL_INPUT',
+	LOGIN_EMAIL_BLUR: 'LOGIN_EMAIL_BLUR',
 };
 
 
 function createInit() {
-	return { type: ACTIONS.INIT, data: {} };
+	return { type: ACTIONS.INIT, data: {
+		email: 'jakob',
+		focus: {
+			element: 'input.login-mail',
+			cursorPosition: 2,
+		}
+	} };
 }
 
 
+function createLoginEmailInput(element) {
+	return {
+		type: ACTIONS.LOGIN_EMAIL_INPUT,
+		data: {
+			value: element.value,
+			focus: {
+				element: 'input.login-mail',
+				selection: [ element.selectionStart, element.selectionEnd ],
+			}
+		},
+	}
+}
+
+
+function createLoginEmailBlur(element, activeElement) {
+	return {
+		type: ACTIONS.LOGIN_EMAIL_BLUR,
+		data: {
+			value: element.value,
+			focus: {
+				element: '.' + activeElement.classList.join('.'),
+				selection: [ activeElement.selectionStart, activeElement.selectionEnd ],
+			}
+		},
+	}
+}
+
 export function init() {
 	const store = createStore(reduce);
-	store.subscribe(render);
-	store.dispatch(createInit);
+	store.subscribe(render.bind(undefined, store));
+	store.dispatch(createInit());
 }
 
 
@@ -25,18 +60,52 @@ export function reduce(state, { type, data }) {
 		case ACTIONS.INIT:
 			state = data;
 			break;
+		case ACTIONS.LOGIN_EMAIL_INPUT:
+			state.email = data.value;
+			state.focus = data.focus;
+			break;
+		case ACTIONS.LOGIN_EMAIL_BLUR:
+			state.email = data.value;
+			state.focus = undefined;
+		case ACTIONS.REGISTER_CLICK:
+			state.validationErrors = [ 'yes' ];
 		default:
 	}
 	return state;
 }
 
 
-function render() {
-	var mountNode = document.getElementById("app");
-	ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
+function render(store) {
+	const mountNode = document.getElementById("app");
+	const name = 'Jakob';
+	const { focus, email } = store.getState();
+	mountNode.innerHTML = loginForm({ email });
+	mountNode.querySelector('input.login-mail').oninput = (e) => {
+		store.dispatch(createLoginEmailInput(e.target));
+	};
+	mountNode.querySelector('input.login-mail').onblur = (e) => {
+		store.dispatch(createLoginEmailBlur(e.target, e.relatedTarget));
+	};
+	if (focus) {
+		mountNode.querySelector(focus.element).focus();
+		mountNode.querySelector(focus.element).setSelectionRange(
+			...focus.selection);
+	}
 }
 
 
-function HelloMessage({ name }) {
-    return <div>Hello {name}</div>;
+function loginForm({ email }) {
+	return `<form>
+		<label>Email: ${inputName(email)}</label>
+		<label>Password: ${inputPassword()}</label>
+		<input type="submit" value="Register"/>
+	</form>`;
+}
+
+function inputName(email) {
+	return `<input class="login-mail" type="text" value="${email}"/>`;
+}
+
+function inputPassword() {
+	return `<input class="login-password" type="password" />`;
 }
