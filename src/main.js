2520424d1307630c4473095c32dca4ebaca95791
Jakob Fuhrmann <3326760+Jafu@users.noreply.github.com>
2020-09-14 09:53:51 +0200

    Make submit button interactive

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-// FIXME make submit button interactive
 // FIXME extract components into module
 // FIXME classes for icons and input validation hints
 // FIXME remove focus handling from actions and reducers
