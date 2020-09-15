3c06b452f3f8aad0807f55edceea8a22e479c9ba
Jakob Fuhrmann <3326760+Jafu@users.noreply.github.com>
2020-09-15 13:00:02 +0200

    clean main function fo a fresh start

--- src/main.js
+++ src/main.js
@@ -1,42 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from 'redux';
-
-
-export const ACTIONS = {
-	INIT: 'INIT',
-};
-
-
-function createInit() {
-	return { type: ACTIONS.INIT, data: {} };
-}
-
-
 export function init() {
-	const store = createStore(reduce);
-	store.subscribe(render);
-	store.dispatch(createInit);
+	console.log('hello world');
 }
 
-
-export function reduce(state, { type, data }) {
-	switch(type) {
-		case ACTIONS.INIT:
-			state = data;
-			break;
-		default:
-	}
-	return state;
-}
-
-
-function render() {
-	var mountNode = document.getElementById("app");
-	ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
-}
-
-
-function HelloMessage({ name }) {
-    return <div>Hello {name}</div>;
-}
